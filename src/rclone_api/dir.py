c745bc6a7bae976cece8a6997f7228837488eeb5
zackees <z@zackees.com>
2025-01-28 15:59:42 -0800

    refactor: update Dir class to use DirListing for ls method

--- src/rclone_api/dir.py
+++ src/rclone_api/dir.py
@@ -1,4 +1,4 @@
-from rclone_api.file import File
+from rclone_api.dir_listing import DirListing
 from rclone_api.rpath import RPath
 
 
@@ -8,20 +8,10 @@ class Dir:
     def __init__(self, path: RPath) -> None:
         self.path = path
 
-    def ls(self) -> tuple[list["Dir"], list[File]]:
+    def ls(self) -> DirListing:
         """List files and directories in the given path."""
-        cmd = ["lsjson", "--files-only", "--dirs-only", "--json", str(self.path)]
         assert self.path.rclone is not None
-        cp = self.path.rclone._run(cmd)
-        text = cp.stdout
-        tmp: list[RPath] = RPath.from_json_str(text)
-        for t in tmp:
-            t.set_rclone(self.path.rclone)
-        # dirs = [o for o in out if o.is_dir]
-        # files = [o for o in out if not o.is_dir]
-        dirs = [Dir(p) for p in tmp if p.is_dir]
-        files = [File(p) for p in tmp if not p.is_dir]
-        return dirs, files
+        return self.path.rclone.ls(self.path.path)
 
     def __str__(self) -> str:
         return str(self.path)
