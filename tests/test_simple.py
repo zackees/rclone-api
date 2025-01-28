9caa71c823800ecd31630109fe61112be5c48b8c
zackees <z@zackees.com>
2025-01-28 14:58:20 -0800

    chore: add initial project setup files and configurations

---
+++ tests/test_simple.py
@@ -0,0 +1,16 @@
+"""
+Unit test file.
+"""
+
+import unittest
+
+
+class RcloneTests(unittest.TestCase):
+    """Test rclone functionality."""
+
+    def test_list_remotes(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
