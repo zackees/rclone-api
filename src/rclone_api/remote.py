a5338e29220e8a9c23ad9771c2b3ae757a229768
zackees <z@zackees.com>
2025-01-28 16:44:33 -0800

    chore: bump version to 1.0.11 and improve type hinting

--- src/rclone_api/remote.py
+++ src/rclone_api/remote.py
@@ -1,12 +1,15 @@
-from rclone_api.rclone import Rclone
+from typing import Any
 
 
 class Remote:
     """Remote (root) directory."""
 
-    def __init__(self, name: str, rclone: Rclone) -> None:
+    def __init__(self, name: str, rclone: Any) -> None:
+        from rclone_api.rclone import Rclone
+
+        assert isinstance(rclone, Rclone)
         self.name = name
-        self.rclone = rclone
+        self.rclone: Rclone = rclone
 
     def __str__(self) -> str:
         return f"{self.name}:"
