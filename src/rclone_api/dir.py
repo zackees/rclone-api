2881b9bc10e1413841f7a4f29dff93657f8dc540
zackees <z@zackees.com>
2025-01-28 16:33:31 -0800

    feat: bump version to 1.0.9 and add walk method to Dir class

--- src/rclone_api/dir.py
+++ src/rclone_api/dir.py
@@ -1,5 +1,8 @@
+from typing import Generator
+
 from rclone_api.dir_listing import DirListing
 from rclone_api.rpath import RPath
+from rclone_api.walk import walk
 
 
 class Dir:
@@ -12,6 +15,11 @@ class Dir:
         """List files and directories in the given path."""
         assert self.path.rclone is not None
         return self.path.rclone.ls(self.path.path, max_depth=max_depth)
+
+    def walk(self, max_depth: int = -1) -> Generator[DirListing, None, None]:
+        """List files and directories in the given path."""
+        assert self.path.rclone is not None
+        return walk(self, max_depth=max_depth)
 
     def __str__(self) -> str:
         return str(self.path)
