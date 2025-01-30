b4402d922b6976970de8e3bf30bea7b0b1ae3152
zackees <z@zackees.com>
2025-01-29 17:23:41 -0800

    feat: implement multithreaded directory walking

--- src/rclone_api/walk.py
+++ src/rclone_api/walk.py
@@ -1,4 +1,5 @@
-from queue import Queue
+from queue import Empty, Queue
+from threading import Thread
 from typing import Generator
 
 from rclone_api import Dir
@@ -6,9 +7,32 @@ from rclone_api.dir_listing import DirListing
 from rclone_api.remote import Remote
 
 
-def walk(
-    dir: Dir | Remote, max_depth: int = -1, check=True
-) -> Generator[DirListing, None, None]:
+def _walk_runner(
+    queue: Queue[Dir], max_depth: int, out_queue: Queue[DirListing | None]
+) -> None:
+    try:
+        while not queue.empty():
+            current_dir = queue.get()
+            dirlisting = current_dir.ls()
+            # yield dirlisting
+            out_queue.put(dirlisting)
+            dirs = dirlisting.dirs
+
+            if max_depth != 0 and len(dirs) > 0:
+                for child in dirs:
+                    queue.put(child)
+            if max_depth < 0:
+                continue
+            if max_depth > 0:
+                max_depth -= 1
+        out_queue.put(None)
+    except KeyboardInterrupt:
+        import _thread
+
+        _thread.interrupt_main()
+
+
+def walk(dir: Dir | Remote, max_depth: int = -1) -> Generator[DirListing, None, None]:
     """Walk through the given directory recursively.
 
     Args:
@@ -18,23 +42,30 @@ def walk(
     Yields:
         DirListing: Directory listing for each directory encountered
     """
-    # Convert Remote to Dir if needed
-    if isinstance(dir, Remote):
-        dir = Dir(dir)
+    try:
+        # Convert Remote to Dir if needed
+        if isinstance(dir, Remote):
+            dir = Dir(dir)
 
-    queue: Queue[Dir] = Queue()
-    queue.put(dir)
+        in_queue: Queue[Dir] = Queue()
+        out_queue: Queue[DirListing] = Queue()
+        in_queue.put(dir)
 
-    while not queue.empty():
-        current_dir = queue.get()
-        dirlisting = current_dir.ls()
-        yield dirlisting
-        dirs = dirlisting.dirs
+        # Start worker thread
+        worker = Thread(
+            target=_walk_runner, args=(in_queue, max_depth, out_queue), daemon=True
+        )
+        worker.start()
 
-        if max_depth != 0 and len(dirs) > 0:
-            for child in dirs:
-                queue.put(child)
-        if max_depth < 0:
-            continue
-        if max_depth > 0:
-            max_depth -= 1
+        while worker.is_alive():
+            try:
+                dirlisting = out_queue.get()
+                if dirlisting is None:
+                    break
+                yield dirlisting
+            except Empty:
+                continue
+
+        worker.join()
+    except KeyboardInterrupt:
+        pass
