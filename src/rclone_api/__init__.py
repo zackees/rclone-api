633e4a6a5a0d03d23eb3acb0b2beed7a717d32b2
zackees <z@zackees.com>
2025-01-29 21:23:34 -0800

    feat: add FileList to public API surface

--- src/rclone_api/__init__.py
+++ src/rclone_api/__init__.py
@@ -2,8 +2,18 @@ from .config import Config
 from .dir import Dir
 from .dir_listing import DirListing
 from .file import File
+from .filelist import FileList
 from .rclone import Rclone
 from .remote import Remote
 from .rpath import RPath
 
-__all__ = ["Rclone", "File", "Config", "Remote", "Dir", "RPath", "DirListing"]
+__all__ = [
+    "Rclone",
+    "File",
+    "Config",
+    "Remote",
+    "Dir",
+    "RPath",
+    "DirListing",
+    "FileList",
+]
