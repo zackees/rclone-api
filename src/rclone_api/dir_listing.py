07f5cff078a076d2ebab854e69d1b00eaa3b9208
zackees <z@zackees.com>
2025-02-03 20:49:54 -0800

    feat: add deduplication function for RPath items

--- src/rclone_api/dir_listing.py
+++ src/rclone_api/dir_listing.py
@@ -1,6 +1,20 @@
 import json
+import warnings
 
 from rclone_api.rpath import RPath
+
+
+def _dedupe(items: list[RPath]) -> list[RPath]:
+    """Remove duplicate items from a list of RPath objects."""
+    seen = set()
+    unique_items = []
+    for item in items:
+        if item not in seen:
+            seen.add(item)
+            unique_items.append(item)
+        else:
+            warnings.warn(f"Duplicate item found: {item}, filtered out.")
+    return unique_items
 
 
 class DirListing:
@@ -9,6 +23,8 @@ class DirListing:
     def __init__(self, dirs_and_files: list[RPath]) -> None:
         from rclone_api.dir import Dir
         from rclone_api.file import File
+
+        dirs_and_files = _dedupe(dirs_and_files)
 
         self.dirs: list[Dir] = [Dir(d) for d in dirs_and_files if d.is_dir]
         self.files: list[File] = [File(f) for f in dirs_and_files if not f.is_dir]
