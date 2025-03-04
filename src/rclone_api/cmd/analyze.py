4782a1996592876d611c4f3b43a7d9c1bd4a54c4
zackees <z@zackees.com>
2025-03-03 18:25:36 -0800

    feat: add analyze script to list files in a remote path

---
+++ src/rclone_api/cmd/analyze.py
@@ -0,0 +1,49 @@
+import argparse
+from dataclasses import dataclass
+from pathlib import Path
+
+from rclone_api import Rclone
+
+
+@dataclass
+class Args:
+    config: Path
+    path: str
+
+    def __post_init__(self):
+        if not self.config.exists():
+            raise FileNotFoundError(f"Config file not found: {self.config}")
+
+
+def list_files(rclone: Rclone, path: str):
+    """List files in a remote path."""
+    for file_item in rclone.ls_stream_files(path, fast_list=True):
+        print(file_item.path, "", file_item.size, file_item.mod_time)
+
+
+def _parse_args() -> Args:
+    parser = argparse.ArgumentParser(description="List files in a remote path.")
+    parser.add_argument(
+        "--config", help="Path to rclone config file", type=Path, default="rclone.conf"
+    )
+    parser.add_argument("path", help="Remote path to list")
+    tmp = parser.parse_args()
+    return Args(config=tmp.config, path=tmp.path)
+
+
+def main() -> int:
+    """Main entry point."""
+    args = _parse_args()
+    path = args.path
+    rclone = Rclone(Path(args.config))
+    list_files(rclone, path)
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    cwd = Path(".").absolute()
+    print(f"cwd: {cwd}")
+    sys.argv.append("dst:TorrentBooks")
+    main()
--- src/rclone_api/file.py
+++ src/rclone_api/file.py
@@ -5,27 +5,54 @@ from pathlib import Path
 
 from rclone_api.rpath import RPath
 
+_STRING_INTERNER: dict[str, str] = {}
+
+
+def _intern(s: str) -> str:
+    return _STRING_INTERNER.setdefault(s, s)
+
 
 # File is too complex, this is a simple dataclass that can be streamed out.
 @dataclass
 class FileItem:
     """Remote file dataclass."""
 
-    path: str
+    parent: str
     name: str
     size: int
     mime_type: str
     mod_time: str
 
+    @property
+    def path(self) -> str:
+        return f"{self.parent}/{self.name}"
+
+    @property
+    def suffix(self) -> str:
+        return self._suffix
+
+    def __post_init__(self):
+        self.parent = _intern(self.parent)
+        self.mime_type = _intern(self.mime_type)
+        suffix = Path(self.name).suffix
+        self._suffix = _intern(suffix)
+
     @staticmethod
     def from_json(data: dict) -> "FileItem | None":
         try:
+            path_str: str = data["Path"]
+            parent_path = Path(path_str).parent.as_posix()
+            name = data["Name"]
+            size = data["Size"]
+            mime_type = data["MimeType"]
+            mod_time = data["ModTime"]
+
             return FileItem(
-                data["Path"],
-                data["Name"],
-                data["Size"],
-                data["MimeType"],
-                data["ModTime"],
+                parent=parent_path,
+                name=name,
+                size=size,
+                mime_type=mime_type,
+                mod_time=mod_time,
             )
         except KeyError:
             warnings.warn(f"Invalid data: {data}")
