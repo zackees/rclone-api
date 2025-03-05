a765856fe40fb567ccf081fa97595ea113725d46
zackees <z@zackees.com>
2025-03-04 18:40:47 -0800

    feat: add fast_list option to fill_db function

--- src/rclone_api/cmd/save_to_db.py
+++ src/rclone_api/cmd/save_to_db.py
@@ -28,17 +28,18 @@ class Args:
     config: Path
     path: str
     db_url: str
+    fast_list: bool
 
     def __post_init__(self):
         if not self.config.exists():
             raise FileNotFoundError(f"Config file not found: {self.config}")
 
 
-def fill_db(rclone: Rclone, path: str):
+def fill_db(rclone: Rclone, path: str, fast_list: bool) -> None:
     """List files in a remote path."""
     # db = DB(_db_url_from_env_or_raise())
     db_url = _db_url_from_env_or_raise()
-    rclone.save_to_db(src=path, db_url=db_url, fast_list=True)
+    rclone.save_to_db(src=path, db_url=db_url, fast_list=fast_list)
 
 
 def _parse_args() -> Args:
@@ -47,12 +48,14 @@ def _parse_args() -> Args:
         "--config", help="Path to rclone config file", type=Path, default="rclone.conf"
     )
     parser.add_argument("--db", help="Database URL", type=str, default=None)
+    parser.add_argument("--fast-list", help="Use fast list", action="store_true")
     parser.add_argument("path", help="Remote path to list")
     tmp = parser.parse_args()
     return Args(
         config=tmp.config,
         path=tmp.path,
         db_url=tmp.db if tmp.db is not None else _db_url_from_env_or_raise(),
+        fast_list=tmp.fast_list,
     )
 
 
@@ -61,7 +64,7 @@ def main() -> int:
     args = _parse_args()
     path = args.path
     rclone = Rclone(Path(args.config))
-    fill_db(rclone, path)
+    fill_db(rclone=rclone, path=path, fast_list=args.fast_list)
     return 0
 
 
